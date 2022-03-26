3af1bea68e14b1bf34644a6cd7c927ae4d36a998
Capwonderbread <hectcast10@yahoo.com>
2022-03-26 02:29:19 -0700

    added save fucntion for timeblock

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
 $("#currentDay").text(moment().format('MMM Do YYYY'));
+var saveBtn = $('#saveBtn');
+
+var colorChange = function() {
+  var thisHour = moment().hours();
+  $(".time-block").each(function() {
+    var timeblockHour = parseInt($(this).attr("id").split("=")[i]);
+
+    if (thisHour > timeblockHour) {
+      $(this).addClass("past");
+    }
+    else if (thisHour === timeblockHour) {
+      $(this).addClass("present");
+    }
+    else {
+      $(this).addClass("past");
+    }
+  });
+};
+
+saveBtn.on("click", function() {
+  var time = $(this).siblings(".hour").text();
+  var event = $(this).siblings("description");
+
+  localStorage.setItem(time, event);
+});
+
